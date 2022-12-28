5cebd6a5b7bb885c4b566424a5e773cda4d2e03c
jake.toan <jake.toan@signifyd.com>
2022-12-28 23:21:04 +0000

    Tilemap impl

--- src/world.rs
+++ src/world.rs
@@ -1,11 +1,13 @@
 use bevy::app::{App, Plugin};
 use bevy::prelude::*;
+use bevy_ecs_tilemap::helpers;
+use bevy_ecs_tilemap::prelude::*;
 
 pub struct WorldPlugin;
 
 impl Plugin for WorldPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(add_camera);
+        app.add_plugin(TilemapPlugin).add_startup_system(startup);
     }
 
     fn name(&self) -> &str {
@@ -17,8 +19,63 @@ impl Plugin for WorldPlugin {
     }
 }
 
-fn add_camera(mut commands: Commands) {
+fn startup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    #[cfg(all(not(feature = "atlas"), feature = "render"))] array_texture_loader: Res<
+        ArrayTextureLoader,
+    >,
+) {
     commands.spawn(Camera2dBundle::default());
-}
 
-fn generate_map() {}
+    let texture_handle: Handle<Image> = asset_server.load("sand.png");
+
+    let map_size = TilemapSize { x: 128, y: 128 };
+
+    let tilemap_entity = commands.spawn_empty().id();
+
+    let mut tile_storage = TileStorage::empty(map_size);
+
+    // TODO maybe just use this?
+    // helpers::filling::fill_tilemap()
+
+    for x in 0..map_size.x {
+        for y in 0..map_size.y {
+            let tile_pos = TilePos { x, y };
+            let tile_entity = commands
+                .spawn(TileBundle {
+                    position: tile_pos,
+                    tilemap_id: TilemapId(tilemap_entity),
+                    ..Default::default()
+                })
+                .id();
+            tile_storage.set(&tile_pos, tile_entity);
+        }
+    }
+
+    let tile_size = TilemapTileSize { x: 100.0, y: 100.0 };
+    let grid_size = tile_size.into();
+    let map_type = TilemapType::default();
+
+    commands.entity(tilemap_entity).insert(TilemapBundle {
+        grid_size,
+        map_type,
+        size: map_size,
+        storage: tile_storage,
+        texture: TilemapTexture::Single(texture_handle),
+        tile_size,
+        transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0),
+        ..Default::default()
+    });
+
+    // Add atlas to array texture loader so it's preprocessed before we need to use it.
+    // Only used when the atlas feature is off and we are using array textures.
+    #[cfg(all(not(feature = "atlas"), feature = "render"))]
+    {
+        array_texture_loader.add(TilemapArrayTexture {
+            texture: TilemapTexture::Single(asset_server.load("tiles.png")),
+            tile_size,
+            ..Default::default()
+        });
+    }
+}
